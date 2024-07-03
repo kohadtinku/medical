d12fadcf380ebc2697798f342a45ed63077651d2
tinku kohad <tinkukohad05@gmail.com>
2024-07-03 16:06:29 +0530

    change ui

--- src/components/Blog.jsx
+++ src/components/Blog.jsx
@@ -21,28 +21,28 @@ const Blog = () => {
           <div class="bg-white cursor-pointer rounded overflow-hidden shadow-[0_2px_10px_-3px_rgba(6,81,237,0.3)] relative top-0 hover:-top-2 transition-all duration-300" data-aos="fade-right">
             <img src={img} alt="Blog Post 1" class="w-[550px] h-[250px] object-cover" />
             <div class="p-6">
-              <span class="text-sm block text-gray-400 mb-2">10 FEB 2023 | BY JOHN DOE</span>
+              <span class="text-sm block text-gray-400 mb-2">Telicor-CT 6.25/40"</span>
               <h3 class="text-xl font-bold text-[#333]">Telmisartan & Chlorthalidone Tables I.P</h3>
             </div>
           </div>
           <div class="bg-white cursor-pointer rounded overflow-hidden shadow-[0_2px_10px_-3px_rgba(6,81,237,0.3)] relative top-0 hover:-top-2 transition-all duration-300" data-aos="fade-up">
             <img src={img1} alt="Blog Post 2" class="w-[550px] h-[250px] object-cover" />
             <div class="p-6">
-              <span class="text-sm block text-gray-400 mb-2">7 JUN 2023 | BY MARK ADAIR</span>
+              <span class="text-sm block text-gray-400 mb-2">Telicor-40</span>
               <h3 class="text-xl font-bold text-[#333]">Telmisartan Tablets I.P</h3>
             </div>
           </div>
           <div class="bg-white cursor-pointer rounded overflow-hidden shadow-[0_2px_10px_-3px_rgba(6,81,237,0.3)] relative top-0 hover:-top-2 transition-all duration-300" data-aos="fade-down">
             <img src={img2} alt="Blog Post 3" class="w-[550px] h-[250px] object-cover" />
             <div class="p-6">
-              <span class="text-sm block text-gray-400 mb-2">5 OCT 2023 | BY SIMON KONECKI</span>
+              <span class="text-sm block text-gray-400 mb-2">Telicor-CT 40/12.5mg</span>
               <h3 class="text-xl font-bold text-[#333]">Telmisartan & Chlortalidone Tablets</h3>
             </div>
           </div>
           <div class="bg-white cursor-pointer rounded overflow-hidden shadow-[0_2px_10px_-3px_rgba(6,81,237,0.3)] relative top-0 hover:-top-2 transition-all duration-300" data-aos="fade-down">
             <img src={img3} alt="Blog Post 3" class="w-[550px] h-[250px] object-cover" />
             <div class="p-6">
-              <span class="text-sm block text-gray-400 mb-2">5 OCT 2023 | BY SIMON KONECKI</span>
+              <span class="text-sm block text-gray-400 mb-2">Telicor-AM 40/5</span>
               <h3 class="text-xl font-bold text-[#333]">Telmisartan & Amlodipine Tablets</h3>
             </div>
           </div>
