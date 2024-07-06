bf748a74ad139f6cef18961d945c58aba496d8c9
tinku kohad <tinkukohad05@gmail.com>
2024-07-06 14:10:55 +0530

    update

--- src/components/MainSlider.jsx
+++ src/components/MainSlider.jsx
@@ -6,7 +6,7 @@ import { Autoplay, Navigation } from "swiper/modules";
 import img1 from "../assets/home_bg.jpg";
 // import img2 from "../assets/home_bg1.jpg";
 import img3 from "../assets/home_bg2.jpg";
-import cord from '../assets/cord.png'
+import cord from "../assets/cord.png";
 
 import AOS from "aos";
 import "aos/dist/aos.css";
@@ -46,11 +46,29 @@ const MainSlider = () => {
       </Swiper>
 
       <div className="absolute inset-0 flex items-center justify-center text-start z-30 c_main ">
-        <div className="text-white p-4 bg-black bg-opacity-20 rounded-md c_main1" data-aos="zoom-in">
-          <h2 className="text-3xl flex gap-1 text-red-500 font-bold" data-aos="fade-right">Welcome To 
-          <img src={cord} className="w-[220px]" alt="" />
-          </h2>
-          <p className="mt-2 text-white font-bold text-lg c_p " data-aos="fade-left">Cordium Pharmaceuticals Pvt. Ltd. has a dedicated team of experienced professionals who work tirelessly to develop innovative medicines that improve patients’ lives. Their commitment to quality, affordability, and accessibility has made them a trusted name in the pharmaceutical industry</p>
+        <div
+          className="text-white p-4 bg-black bg-opacity-20 rounded-md c_main1"
+          data-aos="zoom-in"
+        >
+          <div className="c-main2">
+            <h2
+              className="text-3xl flex gap-1 text-red-800 font-extrabold"
+              data-aos="fade-right"
+            >
+              Welcome To
+              <img src={cord} className="w-[220px]" alt="" />
+            </h2>
+          </div>
+          <p
+            className="mt-2 text-white font-bold text-lg c_p "
+            data-aos="fade-left"
+          >
+            Cordium Pharmaceuticals Pvt. Ltd. has a dedicated team of
+            experienced professionals who work tirelessly to develop innovative
+            medicines that improve patients’ lives. Their commitment to quality,
+            affordability, and accessibility has made them a trusted name in the
+            pharmaceutical industry
+          </p>
           {/* <p className="mt-2 text-white font-bold text-lg c_p " data-aos="fade-left">Our company specializes in the global trade of pharmaceuticals, ensuring the seamless import and export of high-quality medicines. We adhere to international regulations and standards, providing timely and safe delivery of essential medications. Our extensive network and robust logistics guarantee efficient supply chain management, catering to diverse healthcare needs worldwide.</p> */}
         </div>
       </div>
