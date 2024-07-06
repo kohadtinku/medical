b7c556d43307835bb96286abb16a9246653bce6d
tinku kohad <tinkukohad05@gmail.com>
2024-07-06 14:43:09 +0530

    update

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -217,7 +217,7 @@ const Header = () => {
                 </svg>
               </button>
             </div>
-            <div className="flex flex-col p-4 space-y-4 mt-5">
+            <div className="flex flex-col p-4 space-y-4 mt-10">
               <Link
                 to="/"
                 className="text-black hover:text-blue-500 cursor-pointer transition-colors duration-300 uppercase"
